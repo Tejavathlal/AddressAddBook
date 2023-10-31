37cd8564aa93c2fbe3687ba8653101beeb30d34a
tejavathlal <tejavathlal@gmail.com>
2023-10-31 17:34:13 +0530

    toolbar and sidenav bar complete

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './Component/home/home.component';
+import { ContractComponent } from './Component/contract/contract.component';
+import { NewContractComponent } from './Component/new-contract/new-contract.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {path: '', component: HomeComponent},
+  {path: 'contract', component: ContractComponent},
+  {path: 'newcontract', component: NewContractComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,33 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomeComponent } from './Component/home/home.component';
+import { ContractComponent } from './Component/contract/contract.component';
+import { NewContractComponent } from './Component/new-contract/new-contract.component';
+
+// Angular Material
+
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+import {MatSidenavModule} from '@angular/material/sidenav';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ContractComponent,
+    NewContractComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+    MatSidenavModule
   ],
   providers: [],
   bootstrap: [AppComponent]
